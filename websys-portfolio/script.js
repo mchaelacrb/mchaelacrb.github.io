7255b8fa887fa46e4aec2dcbea869ed602676780
Mecha Curiba <110165208+mchaelacrb@users.noreply.github.com>
2024-04-20 11:42:28 +0800

    Delete websys-portfolio directory

--- websys-portfolio/script.js
+++
@@ -1,7 +0,0 @@
-function toggleCards() {
-    var hiddenCards = document.querySelectorAll('.work-card.hidden');
-    hiddenCards.forEach(function(card) {
-      card.classList.toggle('hidden');
-    });
-  }
-  
